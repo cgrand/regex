3154b18c7ab626d873bd2d664ad0f4924993eb3f
Christophe Grand <christophe@cgrand.net>
2012-05-29 07:42:47 +0200

    Adds support for negative and positive lookahead, charsets and predefined char classes. matc-empty? and firsts are not yet used.

--- src/net/cgrand/regex.clj
+++ src/net/cgrand/regex.clj
@@ -4,13 +4,16 @@
   {:author "Christophe Grand"}
   (:refer-clojure :exclude [repeat + * resolve])
   (:require [clojure.core :as core]
-            [clojure.string :as s])
+            [clojure.string :as s]
+            [net.cgrand.regex.charset :as cs])
   (:import [java.util.regex Pattern]))
 
 ;; Value-based DSL definition
 (defprotocol RegexValue
   (pattern [this] "Returns the pattern represented by this value.")
-  (groupnames [this] "Returns a seq of all group names used in this regex."))
+  (groupnames [this] "Returns a seq of all group names used in this regex.")
+  (match-empty? [this])
+  (firsts [this]))
 
 (defrecord Regex [^Pattern re groupnames]
   RegexValue
@@ -31,20 +34,31 @@
     (pattern [c]
       (pattern (str c)))
     (groupnames [this] [])
+    (match-empty? [this] false)
+    (firsts [c] [(cs/charset c)])
   String ; a String denotes a literal sequence of characters to match 
     (pattern [s]
       (Pattern/quote s))
     (groupnames [this] [])
+    (match-empty? [s] (= "" s))
+    (firsts [s] [(cs/charset (first s))])
   clojure.lang.IPersistentSet ; a Set denotes an alternative
     (pattern [set]
       (str "(?:" (s/join "|" (map pattern set)) ")"))
     (groupnames [set] 
       (mapcat groupnames set))
+    (match-empty? [s] (some match-empty? s))
   clojure.lang.ISeq ; a seq denotes a non-capturing group
     (pattern [v]
       (s/join (map pattern v)))
     (groupnames [v] 
       (mapcat groupnames v))
+    (match-empty? [v] (every? match-empty? v))
+    (firsts [s]
+      (when-let [[x & xs] (seq s)]
+        (if (match-empty? x)
+          (firsts #{x xs})
+          (firsts x))))
   clojure.lang.IPersistentVector ; a Vector denotes a group (capturing or not)
     (pattern [v]
       (if (-> v rseq second (= :as)) 
@@ -54,13 +68,37 @@
       (if (-> v rseq second (= :as))
         (cons (peek v) (groupnames (-> v pop pop seq)))
         (groupnames (seq v))))
-  clojure.lang.IPersistentMap ; a map denotes a char range
+    (match-empty? [v] 
+      (match-empty? (seq (if (-> v rseq second (= :as))
+                           (-> v pop pop)
+                           v))))
+    (firsts [v] 
+      (firsts (seq (if (-> v rseq second (= :as))
+                     (-> v pop pop)
+                     v))))
+  clojure.lang.APersistentMap ; a map denotes a char range
     (pattern [m]
-      (let [esc {\] "\\x5D" \[ "\\x5B" \& "\\x26" \^ "\\x5E"}]
-        (apply str (concat ["["] 
-                     (mapcat (fn [[k v]]
-                               [(esc k k) \- (esc v v)]) m) ["]"]))))
+      (pattern (cs/charset m)))
     (groupnames [v] [])
+    (match-empty? [this] false)
+    (firsts [m] (firsts (cs/charset m)))
+  net.cgrand.regex.charset.Charset
+    (pattern [cs]
+      (let [reserved (set "[]&^-")
+            esc #(if (and (not (< 0x1F (int %) 0x7F)) (reserved %))
+                   %
+                   (format "\\u%04X" (int %)))
+            cs (-> cs cs/charset cs/ranges)]
+        (apply str (concat ["["] 
+                           (mapcat (fn [[a b]]
+                                     (if (and a (= a b))
+                                       [(esc a)]
+                                       [(esc (or a \u0000)) "-" 
+                                        (esc (or b \uFFFF))])) cs) 
+                           ["]"]))))
+    (groupnames [v] [])
+    (match-empty? [this] false)
+    (firsts [this] [this])
   ; I haven't found a way to get the number of groups of a given pattern
   #_Pattern
     #_(pattern [re] (.pattern re))
@@ -77,7 +115,9 @@
             min (or min 0)]
         (str "(?:" s "){" min "," max "}")))
     (groupnames [this] 
-      (groupnames frag)))
+      (groupnames frag))
+    (match-empty? [this] (or (zero? min) (match-empty? frag)))
+    (firsts [this] (firsts frag)))
 
 (defn repeat 
  ([spec] (Repeat. spec nil nil))  
@@ -102,9 +142,52 @@
       (pattern [this] ".")
       (groupnames [this] [])))
 
-;; at this point the DSL is fully functional and everything else is just 
-;; compile-time optimizations -- optimizations that can be disabled by setting
-;; *optimize* to false
+(defrecord PositiveLookahead [frag]
+  RegexValue
+  (pattern [this]
+    (str "(?=" (pattern frag) ")"))
+  (groupnames [this]
+    (groupnames frag)))
+
+(defn ?= [frag] (PositiveLookahead. frag))
+
+(defrecord NegativeLookahead [frag]
+  RegexValue
+  (pattern [this]
+    (str "(?!" (pattern frag) ")"))
+  (groupnames [this]
+    nil))
+
+(defn ?! [frag] (NegativeLookahead. frag))
+
+;; predefined classes
+(def digit {\0 \9})
+(def !digit (cs/- digit))
+(def space (cs/charset " \t\n\u000B\f\r"))
+(def !space (cs/- space))
+(def wordchar {\a \z \A \Z \_ \_ \0 \9})
+(def !wordchar (cs/- wordchar))
+
+(defmacro letmap [& bindings]
+  `(let [~@bindings]
+     ~(let [syms (take-nth 2 bindings)]
+        (zipmap (map keyword syms) syms))))
+
+(def posix
+  (letmap 
+    Lower {\a \z}
+    Upper {\A \Z}
+    ASCII {\u0000 \u007F}
+    Alpha (cs/+ Lower Upper)
+    Digit {\0 \9}
+    Alnum (cs/+ Alpha Digit)
+    Punct (cs/charset "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    Graph (cs/+ Alnum Punct)
+    Print (cs/+ Graph \space)
+    Blank (cs/charset " \t")
+    Cntrl (cs/+ {\u0000 \u001F} \u007F)
+    XDigit {\0 \9 \a \f \A \F}
+    Space (cs/charset " \t\n\u000B\f\r")))
 
 (comment 
   regex=> (exec (regex [(repeat {\a \z}) :as :a] " " [(repeat {\a \z}) :as :b]) 
