3e83c7f11f29a2b217452d954c529d66417fc4b7
Christophe Grand <christophe@cgrand.net>
2012-06-05 15:33:07 +0200

    Adds derivation to regexes

--- src/net/cgrand/regex.clj
+++ src/net/cgrand/regex.clj
@@ -2,7 +2,7 @@
   "A DSL for people who prefer verbose, maintenable regexes to terse 
    now-you-have-two-problems ones."
   {:author "Christophe Grand"}
-  (:refer-clojure :exclude [repeat + * resolve])
+  (:refer-clojure :exclude [repeat + * resolve derive])
   (:require [clojure.core :as core]
             [clojure.string :as s]
             [net.cgrand.regex.charset :as cs])
@@ -13,12 +13,21 @@
   (pattern [this] "Returns the pattern represented by this value.")
   (groupnames [this] "Returns a seq of all group names used in this regex.")
   (match-empty? [this])
-  (firsts [this]))
+  (firsts [this] "Returns a collection of disjunct charsets -- for a given 
+    charset, derive must be constant.")
+  (derive [this x]))
 
-(defrecord Regex [^Pattern re groupnames]
+(declare regex*)
+
+(defrecord Regex [^Pattern re groupnames spec]
   RegexValue
     (pattern [this] (.pattern re))
-    (groupnames [this] groupnames))
+    (groupnames [this] groupnames)
+    (match-empty? [this] (match-empty? spec))
+    (firsts [this] (firsts spec))
+    (derive [this x]
+      (when-let [spec (derive spec x)]
+        (regex* spec))))
 
 (defn exec [re s]
   (when-let [res (re-matches (:re re) s)]
@@ -27,7 +36,12 @@
       res)))
 
 (defn regex* [spec]
-  (Regex. (-> spec pattern Pattern/compile) (groupnames spec)))
+  (Regex. (-> spec pattern Pattern/compile) (groupnames spec) spec))
+
+(defn- as-seq [v]
+  (sequence (if (-> v rseq second (= :as))
+              (-> v pop pop)
+              v)))
 
 (extend-protocol RegexValue
   Character
@@ -36,18 +50,28 @@
     (groupnames [this] [])
     (match-empty? [this] false)
     (firsts [c] [(cs/charset c)])
+    (derive [c x]
+      (when (= c x) ""))
   String ; a String denotes a literal sequence of characters to match 
     (pattern [s]
       (Pattern/quote s))
     (groupnames [this] [])
     (match-empty? [s] (= "" s))
     (firsts [s] [(cs/charset (first s))])
+    (derive [s c]
+      (when (= c (first s)) (subs s 1)))
   clojure.lang.IPersistentSet ; a Set denotes an alternative
     (pattern [set]
       (str "(?:" (s/join "|" (map pattern set)) ")"))
     (groupnames [set] 
       (mapcat groupnames set))
-    (match-empty? [s] (some match-empty? s))
+    (match-empty? [set] (some match-empty? set))
+    (firsts [set]
+      (reduce cs/disjunctive-union
+              (map firsts set)))
+    (derive [s c]
+      (when-let [[x & xs :as ds] (seq (keep #(derive % c) s))]
+        (if xs (set ds) x)))
   clojure.lang.ISeq ; a seq denotes a non-capturing group
     (pattern [v]
       (s/join (map pattern v)))
@@ -59,6 +83,16 @@
         (if (match-empty? x)
           (firsts #{x xs})
           (firsts x))))
+    (derive [s c]
+      (when-let [[x & xs] (seq s)]
+        (let [dx (derive x c)
+              dxs (when (and xs (match-empty? x)) (derive xs c))
+              dxxs (when dx (if xs (cons dx xs) dx))]
+          (or
+            ; I have a nagging doubt that the set below may throw a duplicate
+            ; key exception without the not= 
+            (and dxxs dxs (not= dxxs dxs) #{dxxs dxs}) 
+            dxxs dxs))))
   clojure.lang.IPersistentVector ; a Vector denotes a group (capturing or not)
     (pattern [v]
       (if (-> v rseq second (= :as)) 
@@ -69,19 +103,17 @@
         (cons (peek v) (groupnames (-> v pop pop seq)))
         (groupnames (seq v))))
     (match-empty? [v] 
-      (match-empty? (seq (if (-> v rseq second (= :as))
-                           (-> v pop pop)
-                           v))))
+      (match-empty? (as-seq v)))
     (firsts [v] 
-      (firsts (seq (if (-> v rseq second (= :as))
-                     (-> v pop pop)
-                     v))))
+      (firsts (seq (as-seq v))))
+    (derive [v c]
+      (derive (as-seq v) c))
   clojure.lang.APersistentMap ; a map denotes a char range
-    (pattern [m]
-      (pattern (cs/charset m)))
+    (pattern [m] (pattern (cs/charset m)))
     (groupnames [v] [])
     (match-empty? [this] false)
     (firsts [m] (firsts (cs/charset m)))
+    (derives [m c] (derive (cs/charset m) c))
   net.cgrand.regex.charset.Charset
     (pattern [cs]
       (let [reserved (set "[]&^-")
@@ -99,10 +131,14 @@
     (groupnames [v] [])
     (match-empty? [this] false)
     (firsts [this] [this])
-  ; I haven't found a way to get the number of groups of a given pattern
-  #_Pattern
-    #_(pattern [re] (.pattern re))
-    #_(groupnames [re] []))
+    (derives [this c]
+      (when (cs/has? this c) ""))
+  nil
+    (pattern [_] "(?!a)(?=a)")
+    (groupnames [_] [])
+    (match-empty? [_] false)
+    (firsts [_] nil)
+    (derive [_ c] nil))
 
 (defn regex [& specs] 
   (regex* (vec specs)))
@@ -111,16 +147,21 @@
   RegexValue
     (pattern [this] 
       (let [s (pattern frag)
-            max (or max "")
-            min (or min 0)]
+            max (or max "")]
         (str "(?:" s "){" min "," max "}")))
     (groupnames [this] 
       (groupnames frag))
     (match-empty? [this] (or (zero? min) (match-empty? frag)))
-    (firsts [this] (firsts frag)))
+    (firsts [this] (firsts frag))
+    (derive [this c]
+      (when-let [dfrag (derive frag c)]
+        (cond
+          (and max (< min max)) (list dfrag (Repeat. frag min (dec max)))
+          (= min max) dfrag
+          (nil? max) (list dfrag this)))))
 
 (defn repeat 
- ([spec] (Repeat. spec nil nil))  
+ ([spec] (Repeat. spec 0 nil))  
  ([spec min] (Repeat. spec min nil))
  ([spec min max] (Repeat. spec min max)))
 
@@ -198,5 +239,3 @@
   regex=> (exec datestamp-re "20X7-10-23")
   nil
 )
-
-
