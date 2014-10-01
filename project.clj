9e40c5070e485dc8a773db90c5c4e2863fd0f986
Joe Littlejohn <joelittlejohn@gmail.com>
2014-10-01 10:40:13 +0100

    Remove version range for org.clojure/clojure
    Closes #4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject net.cgrand/regex "1.1.0"
   :description "a DSL for people who prefer verbose, composable regexes"
   :url "http://github.com/cgrand/regex/"
-  :dependencies [[org.clojure/clojure "[1.2.0,)"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]])
